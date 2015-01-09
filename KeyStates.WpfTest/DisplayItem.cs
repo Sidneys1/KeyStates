1e03177993a0e60d48bfbcfa47c859f774ab4537
Sidneys1 <sidneys1@live.com>
2015-01-08 21:36:34 -0500

    Missing files

---
+++ KeyStates.WpfTest/DisplayItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Media;
+
+namespace WpfTest
+{
+	public class DisplayItem
+	{
+		public string Text { get; set; }
+		public Color Color { get; set; }
+
+		public DisplayItem(string text, Color color)
+		{
+			Text = text;
+			Color = color;
+		}
+	}
+}
---
+++ KeyStates.WpfTest/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Threading;
+using KeyStates;
+
+namespace WpfTest
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			PassiveKeyboardMonitor.KeyDown += args =>
+			{
+				Dispatcher.Invoke(DispatcherPriority.Input, new KeyEvent(ProcessKeyDown), args);
+			};
+
+			PassiveKeyboardMonitor.KeyUp += args =>
+			{
+				Dispatcher.Invoke(DispatcherPriority.Input, new KeyEvent(ProcessKeyUp), args);
+			};
+
+			PassiveKeyboardMonitor.KeyPressed += args =>
+			{
+				Dispatcher.Invoke(DispatcherPriority.Input, new KeyEvent(ProcessKeyPress), args);
+			};
+
+			PassiveKeyboardMonitor.Start();
+		}
+
+		private void ProcessKeyDown(KeyEventArgs keyEventArgs)
+		{
+			AddItem(string.Format("Key Dw\t{0}", keyEventArgs.Key), Colors.Gray);
+		}
+
+		private void ProcessKeyUp(KeyEventArgs keyEventArgs)
+		{
+			AddItem(string.Format("Key Up\t{0}", keyEventArgs.Key), Colors.Gray);
+		}
+
+		private void ProcessKeyPress(KeyEventArgs keyEventArgs)
+		{
+			AddItem(string.Format("Key Ps\t{0}", keyEventArgs.Key), Colors.Green);
+
+			var text = keyEventArgs.Key.ToChar(PassiveKeyboardMonitor.IsShiftPressed, PassiveKeyboardMonitor.IsAltGrPressed);
+			if (text == '\b')
+				TextBlock.Text = TextBlock.Text.Remove(TextBlock.Text.Length - 1, 1);
+			else if (text != '\0' && text != '\r')
+				TextBlock.Text += text;
+		}
+
+		private void AddItem(string format, Color green)
+		{
+			ListView.Items.Add(new DisplayItem(format, green));
+			ListView.UpdateLayout();
+			ListView.ScrollIntoView(ListView.Items[ListView.Items.Count - 1]);
+		}
+
+		private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+		{
+			PassiveKeyboardMonitor.Stop();
+		}
+	}
+}
